c813be9a0509f06e0b8083424b04427ab0c0ce46
Szabó Gábor <szabogabor@hydrogene.hu>
2017-06-01 12:37:49 +0200

    Starter pack is ready

--- functions.php
+++ functions.php
@@ -10,17 +10,20 @@
  * @link https://github.com/roots/sage/pull/1042
  */
 $sage_includes = [
+  'lib/soil/soil.php', // Soil
   'lib/assets.php',    // Scripts and stylesheets
   'lib/extras.php',    // Custom functions
   'lib/setup.php',     // Theme setup
   'lib/titles.php',    // Page titles
   'lib/wrapper.php',   // Theme wrapper class
   'lib/customizer.php' // Theme customizer
+  'lib/customposts.php', // Custom post Types
+  'lib/custom.php'     // Custom code by ceto
 ];
 
 foreach ($sage_includes as $file) {
   if (!$filepath = locate_template($file)) {
-    trigger_error(sprintf(__('Error locating %s for inclusion', 'sage'), $file), E_USER_ERROR);
+    trigger_error(sprintf(__('Error locating %s for inclusion', 'airon'), $file), E_USER_ERROR);
   }
 
   require_once $filepath;
