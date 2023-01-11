9b4e08f491f63aa2c751c33ae77ae89150ac5bc3
Gabor Szabo <szabogabi@gmail.com>
2023-01-11 22:37:03 +0100

    kicsi

--- template-iframer.php
+++ template-iframer.php
@@ -6,32 +6,36 @@
 <?php while (have_posts()) : the_post(); ?>
 <!-- <?php get_template_part('templates/page', 'header'); ?> -->
 
+
 <?php if ($brochureid = get_field('file') ) : ?>
   <?php
       $file = get_attached_file($brochureid);
       $fileurl = wp_get_attachment_url($brochureid);
       $filetitle = get_the_title($brochureid)?get_the_title($brochureid):basename( $fileurl );
   ?>
+<?php endif; ?>
+
+
+<?php if ($brochureid ) : ?>
   <iframe class="docframe" src="https://docs.google.com/gview?url=<?= $fileurl;?>&embedded=true" frameborder="0"></iframe>
-  <?php endif; ?>
+<?php endif; ?>
 
-  <div class="wrapper ps ps--narrow">
-    <div class="row column">
-        <?php if ($brochureid = get_field('file') ) : ?>
-
-            <a class="banner__cta button large" download href="<?= $fileurl; ?>">
-                <?= $filetitle; ?>
-                <em class="brdlbtn__fileinfo">
-                    <?php _e('Download', 'airon'); ?> <span class="brdlbtn__extension"><?= pathinfo($file, PATHINFO_EXTENSION); ?></span>&nbsp;|&nbsp;<span class="brdlbtn__size"><?= size_format(filesize($file)) ?></span>
-                </em>
-            </a>
-            <br>
-        <?php endif; ?>
-        <div class="lead">
-            <?php the_content(); ?>
-        </div>
-    </div>
+<div class="wrapper ps ps--narrow">
+  <div class="row column">
+      <?php if ($brochureid ) : ?>
+          <a class="banner__cta button large" download href="<?= $fileurl; ?>">
+              <?= $filetitle; ?>
+              <em class="brdlbtn__fileinfo">
+                  <?php _e('Download', 'airon'); ?> <span class="brdlbtn__extension"><?= pathinfo($file, PATHINFO_EXTENSION); ?></span>&nbsp;|&nbsp;<span class="brdlbtn__size"><?= size_format(filesize($file)) ?></span>
+              </em>
+          </a>
+          <br>
+      <?php endif; ?>
+      <div class="lead">
+          <?php the_content(); ?>
+      </div>
   </div>
+</div>
 
 
 
