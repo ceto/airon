aa4c56d851c91e482803262440057e2e7f82ff79
Gabor Szabo <szabogabi@gmail.com>
2024-01-25 21:53:11 +0100

    man menu / services submenu

--- templates/footer.php
+++ templates/footer.php
@@ -5,11 +5,16 @@
             <a href="<?= esc_url(home_url('/')); ?>">
                 <img class="footerlogo" src="<?= get_stylesheet_directory_uri(); ?>/dist/images/logostar.svg" alt="Airon">
             </a>
+            <ul class="footer__specilist">
             <?php
-              if (has_nav_menu('primary_navigation')) :
-                wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'footer__specilist']);
-              endif;
-            ?>
+              $specs = get_terms([
+                'taxonomy' => 'specialization',
+                'hide_empty' => false,
+              ]);
+              foreach ($specs as $key => $speci) : ?>
+              <li><a href="<?= get_term_link($speci) ?>"><?= $speci->name ?></a></li>
+              <?php endforeach; ?>
+            </ul>
             <?php dynamic_sidebar('sidebar-footer'); ?>
         </div>
         <div class="sitefooter__bottom column row expanded">
